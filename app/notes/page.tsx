a67e4a8f14dfc98d021a6f16862fc14784150e32
Iryna-M-html <ivmohnataya@gmail.com>
2025-09-23 21:22:45 +0300

    konspect 1000 oshybok

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,37 @@
-export default function NotesPage() {
+// export default function NotesPage() {
+//   return (
+//     <div>
+//       <h1>Notes page</h1>
+//       <p>This is the Note route in Next.js.</p>
+//     </div>
+//   );
+// }
+
+// // 1. Імпортуємо функцію
+// import { getNotes } from "@/lib/api";
+
+// // 2. Робимо фукнцію асинхронною
+// const Notes = async () => {
+//   // 3. Виконуємо запит
+//   const notes = await getNotes();
+//   console.log("notes", notes);
+
+//   return <div>Notes page</div>;
+// };
+
+// export default Notes;
+import { getNotes } from "@/lib/api";
+import NoteList from "@/components/NoteList/NoteList";
+
+const Notes = async () => {
+  const response = await getNotes();
+
   return (
-    <div>
-      <h1>Notes page</h1>
-      <p>This is the Note route in Next.js.</p>
-    </div>
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
   );
-}
+};
+
+export default Notes;

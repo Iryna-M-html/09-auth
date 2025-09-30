4a13b9eb4098317216446b31a5011f16f6852c52
Iryna-M-html <ivmohnataya@gmail.com>
2025-09-30 20:01:15 +0300

    deleted NotesPageClientProps (types/note.ts)

--- types/note.ts
+++ types/note.ts
@@ -8,8 +8,3 @@ export interface Note {
 }
 
 export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
-
-export type NotesPageClientProps = {
-  initialNotes: Note[];
-  initialTotalPages: number;
-};

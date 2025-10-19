70b10a18efc1193a722efe1fc354855bce547091
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-19 23:33:30 +0300

    lib/api/serverApi.ts

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,6 +1,7 @@
 import { User } from "@/types/user";
 import { nextServerApi, SessionResponse } from "./api";
 import { cookies } from "next/headers";
+import type { Note } from "../../types/note";
 export const getServerMe = async () => {
   const cookieStore = await cookies();
   const { data } = await nextServerApi.get<User>(`/users/me`, {
@@ -15,4 +16,16 @@ export const checkServerSession = async () => {
     headers: { Cookie: cookieStore.toString() },
   });
   return res;
+};
+export const getServerNoteById = async (id: string): Promise<Note> => {
+  if (!id) {
+    throw new Error("Note ID is required");
+  }
+
+  const cookieStore = await cookies();
+  const { data } = await nextServerApi.get<Note>(`/notes/${id}`, {
+    headers: { Cookie: cookieStore.toString() },
+  });
+
+  return data;
 };

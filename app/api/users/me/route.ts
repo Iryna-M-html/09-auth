b99be09fa0dfaecc298d6ee379de0ec97caa5cea
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-19 23:23:23 +0300

    app/api/users/me/route.ts

--- app/api/users/me/route.ts
+++ app/api/users/me/route.ts
@@ -1,21 +1,58 @@
 import { NextResponse } from "next/server";
-import { api, ApiError } from "../../api";
+import { api } from "../../api";
 import { cookies } from "next/headers";
+import { isAxiosError } from "axios";
+import { logErrorResponse } from "@/app/api/auth/utils/logErrorResponse";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
-  console.log("test get");
-  //return NextResponse.json({ message: "ok" });
   const cookieStore = await cookies();
   try {
-    const { data } = await api.get("/users/me", {
+    const apiRes = await api.get("/users/me", {
       headers: { Cookie: cookieStore.toString() },
     });
-    return NextResponse.json(data);
+
+    return NextResponse.json(apiRes.data, { status: apiRes.status });
   } catch (err) {
-    const error = err as ApiError;
+    if (isAxiosError(err)) {
+      logErrorResponse(err);
+      return NextResponse.json(
+        { error: err.response?.data?.error ?? err.message },
+        { status: err.response?.status ?? 500 }
+      );
+    }
+
+    console.error("Unexpected error:", err);
     return NextResponse.json(
-      { error: error.response?.data?.error ?? error.message },
-      { status: error.status }
+      { error: "Unexpected server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request: Request) {
+  const cookieStore = await cookies();
+  const body = await request.json();
+
+  try {
+    const apiRes = await api.patch("/users/me", body, {
+      headers: { Cookie: cookieStore.toString() },
+    });
+
+    return NextResponse.json(apiRes.data, { status: apiRes.status });
+  } catch (err) {
+    if (isAxiosError(err)) {
+      logErrorResponse(err);
+      return NextResponse.json(
+        { error: err.response?.data?.error ?? err.message },
+        { status: err.response?.status ?? 500 }
+      );
+    }
+
+    console.error("Unexpected error:", err);
+    return NextResponse.json(
+      { error: "Unexpected server error" },
+      { status: 500 }
     );
   }
 }

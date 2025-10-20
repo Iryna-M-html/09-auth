6d52a14bef565a70ca32a25b3faf04c0e362d11a
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-20 16:53:43 +0300

    (файл app/api/users/me/route.ts)

--- app/api/users/me/route.ts
+++ app/api/users/me/route.ts
@@ -6,52 +6,53 @@ import { logErrorResponse } from "@/app/api/auth/utils/logErrorResponse";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const cookieStore = await cookies();
   try {
-    const apiRes = await api.get("/users/me", {
-      headers: { Cookie: cookieStore.toString() },
-    });
+    const cookieStore = await cookies();
 
-    return NextResponse.json(apiRes.data, { status: apiRes.status });
-  } catch (err) {
-    if (isAxiosError(err)) {
-      logErrorResponse(err);
+    const res = await api.get("/users/me", {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: err.response?.data?.error ?? err.message },
-        { status: err.response?.status ?? 500 }
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
       );
     }
-
-    console.error("Unexpected error:", err);
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json(
-      { error: "Unexpected server error" },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
 }
 
 export async function PATCH(request: Request) {
-  const cookieStore = await cookies();
-  const body = await request.json();
-
   try {
-    const apiRes = await api.patch("/users/me", body, {
-      headers: { Cookie: cookieStore.toString() },
-    });
+    const cookieStore = await cookies();
+    const body = await request.json();
 
-    return NextResponse.json(apiRes.data, { status: apiRes.status });
-  } catch (err) {
-    if (isAxiosError(err)) {
-      logErrorResponse(err);
+    const res = await api.patch("/users/me", body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
       return NextResponse.json(
-        { error: err.response?.data?.error ?? err.message },
-        { status: err.response?.status ?? 500 }
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
       );
     }
-
-    console.error("Unexpected error:", err);
+    logErrorResponse({ message: (error as Error).message });
     return NextResponse.json(
-      { error: "Unexpected server error" },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }

3c0bc1d88a64dcc5f944d64fe2e287f857bd0334
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-20 16:46:22 +0300

    app/api/auth/session/route.ts)

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -6,48 +6,50 @@ import { isAxiosError } from "axios";
 import { logErrorResponse } from "../../auth/utils/logErrorResponse";
 
 export async function GET() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
+  try {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+    const refreshToken = cookieStore.get("refreshToken")?.value;
 
-  if (accessToken) {
-    return NextResponse.json({ success: true });
-  }
-  if (refreshToken) {
-    try {
-      const apiRes = await api.get("/auth/session", {
+    if (accessToken) {
+      return NextResponse.json({ success: true });
+    }
+
+    if (refreshToken) {
+      const apiRes = await api.get("auth/session", {
         headers: {
           Cookie: cookieStore.toString(),
         },
       });
+
       const setCookie = apiRes.headers["set-cookie"];
 
       if (setCookie) {
         const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
         for (const cookieStr of cookieArray) {
           const parsed = parse(cookieStr);
+
           const options = {
             expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
             path: parsed.Path,
             maxAge: Number(parsed["Max-Age"]),
           };
+
           if (parsed.accessToken)
             cookieStore.set("accessToken", parsed.accessToken, options);
           if (parsed.refreshToken)
             cookieStore.set("refreshToken", parsed.refreshToken, options);
         }
-        return NextResponse.json({ success: true });
+        return NextResponse.json({ success: true }, { status: 200 });
       }
-      return NextResponse.json({ success: false });
-    } catch (err) {
-      if (isAxiosError(err)) {
-        logErrorResponse(err);
-      } else {
-        console.error("Unexpected error:", err);
-      }
-      return NextResponse.json({ success: false });
     }
+    return NextResponse.json({ success: false }, { status: 200 });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json({ success: false }, { status: 200 });
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json({ success: false }, { status: 200 });
   }
-
-  return NextResponse.json({ success: false });
 }
